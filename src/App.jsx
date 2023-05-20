ca09e0c2791a5890f86e8bc23e3ef7809b6b1d1f
Pete Laptop <pmugasa78@gmail.com>
2023-05-20 02:48:49 +0200

    implemented error handling that updates the ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import DragandDrop from "./components/DragandDrop";
 import Button from "./components/Button";
 import Loader from "./components/Loader";
 import Success from "./components/Success";
+import axios from "axios";
 
 function App() {
   const [isUploading, setIsUploading] = useState(false);
   const [isUploaded, setIsUploaded] = useState(false);
+
   const [error, setError] = useState("");
   const formats = ["jpg", "webp", "svg", "png"];
 
@@ -16,7 +18,7 @@ function App() {
     if (error) {
       const timeoutId = setTimeout(() => {
         setError("");
-      }, 2000);
+      }, 5000);
 
       return () => {
         clearTimeout(timeoutId);
@@ -24,18 +26,39 @@ function App() {
     }
   }, [error]);
 
-  function onUpload(image) {
+  async function onUpload(image) {
     setIsUploading(true);
-    const post = () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", image[0]);
+      const response = await axios.post(
+        "http://localhost:3001/images",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setIsUploading(false);
+      setIsUploaded(true);
+    } catch (error) {
+      setIsUploaded(false);
+      setIsUploading(false);
+      setError("Error uploading file." + " " + error.message);
+    }
+    /*const post = () => {
       setTimeout(function () {
-        console.log("posting to server...", image);
+        console.log("posting to server...", image[0]);
 
         setIsUploading(false);
         setIsUploaded(true);
       }, 3000);
     };
-    post();
+    post();*/
   }
+
   if (isUploading) {
     return <Loader />;
   } else if (isUploaded) {
@@ -44,33 +67,35 @@ function App() {
     return (
       <>
         <div className="h-screen w-screen flex flex-col items-center justify-center">
-          <Card>
-            <h3 className="text-center mt-9 font-sans text-base font-medium text-gray">
-              Upload your image
-            </h3>
-            <p className="text-center mt-4 font-sans text-[10px] font-medium text-gray">
-              File should be Jpeg, Png...
-            </p>
-            {error ? (
-              <p className="text-[10px] text-red-500 font-sans font-light p-4 mt-4 border border-red-400 bg-red-200 w-full rounded-sm h-10 flex items-center justify-center">
-                {error}
+          <form encType="multi-part/form-data">
+            <Card>
+              <h3 className="text-center mt-9 font-sans text-base font-medium text-gray">
+                Upload your image
+              </h3>
+              <p className="text-center mt-4 font-sans text-[10px] font-medium text-gray">
+                File should be Jpeg, Png...
               </p>
-            ) : null}
+              {error ? (
+                <p className="text-[10px] text-red-500 font-sans font-light p-4 mt-4 border border-red-400 bg-red-200 w-full rounded-sm h-10 flex items-center justify-center">
+                  {error}
+                </p>
+              ) : null}
 
-            <DragandDrop
-              onUpload={onUpload}
-              setError={setError}
-              setIsUploading={setIsUploading}
-              formats={formats}
-            />
+              <DragandDrop
+                onUpload={onUpload}
+                setError={setError}
+                setIsUploading={setIsUploading}
+                formats={formats}
+              />
 
-            <div className="text-center text-very-light-gray font-sans text-[12px] font-medium mt-9">
-              Or
-            </div>
-            <Button onUpload={onUpload} formats={formats} setError={setError}>
-              Choose file
-            </Button>
-          </Card>
+              <div className="text-center text-very-light-gray font-sans text-[12px] font-medium mt-9">
+                Or
+              </div>
+              <Button onUpload={onUpload} formats={formats} setError={setError}>
+                Choose file
+              </Button>
+            </Card>
+          </form>
           <footer className="mt-4 font-sans font-medium text-gray text-lg">
             Made by Pete✌🏾
           </footer>
